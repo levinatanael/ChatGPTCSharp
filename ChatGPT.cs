f8f44a9dae830a2384c3cad01bfee4a3b4b4363c
Levi Natanael <levi.natanael@gmail.com>
2023-07-26 18:55:04 -0300

    Subindo projeto

---
+++ ChatGPT.cs
@@ -0,0 +1,68 @@
+﻿using OpenAI;
+using OpenAI.Managers;
+using OpenAI.ObjectModels;
+using OpenAI.ObjectModels.RequestModels;
+using System.Configuration;
+
+namespace ChatGPTCSharp
+{
+    public class ChatGPT
+    {
+        public OpenAIService Service { get; private set; }
+
+        //Conexão com o ChatGPT
+        public ChatGPT()
+        {
+            Service = new OpenAIService(new OpenAiOptions()
+            {
+                ApiKey = ConfigurationManager.AppSettings["ApiKeyChatGpt"]
+            });
+        }
+
+        public async Task<string> GenerateText(string prompt)
+        {
+            var completionResult = await Service.Completions.CreateCompletion(new CompletionCreateRequest
+            {
+                Prompt = prompt,
+                Model = Models.TextDavinciV3
+            });
+
+            if (completionResult.Successful)
+            {
+                var result = completionResult.Choices.FirstOrDefault();
+                var text = "Nenhum resultado encontrado.";
+                if (result != null)
+                {
+                    text = result.Text;
+                }
+                return text;
+            }
+
+            return $"Erro: #{completionResult.Error?.Code}: {completionResult.Error?.Message}";
+        }
+
+        public async Task<string> GenerateImage(string prompt)
+        {
+            var imageResult = await Service.Image.CreateImage(new ImageCreateRequest
+            {
+                Prompt = prompt,
+                N = 1,
+                Size = StaticValues.ImageStatics.Size.Size256,
+                ResponseFormat = StaticValues.ImageStatics.ResponseFormat.Url
+            }); ;
+
+            if (imageResult.Successful)
+            {
+                var image = imageResult.Results.FirstOrDefault();
+                var url = "Nenhuma imagem encontrada.";
+                if (image != null)
+                {
+                    url = image.Url;
+                }
+                return url;
+            }
+
+            return $"Erro: #{imageResult.Error?.Code}: {imageResult.Error?.Message}";
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using ChatGPTCSharp;
+
+var chatGpt = new ChatGPT();
+
+Console.WriteLine("Digite um assunto para receber um texto e uma imagem gerada pelo ChatGPT.");
+Console.WriteLine("");
+Console.Write("Assunto: ");
+var assunto = Console.ReadLine();
+
+if (!string.IsNullOrWhiteSpace(assunto))
+{
+    var texto = await chatGpt.GenerateText(assunto);
+    var imagem = await chatGpt.GenerateImage(assunto);
+
+    Console.WriteLine("--");
+    Console.WriteLine("RESULTADOS:");
+    Console.WriteLine("--");
+    Console.WriteLine($"Texto gerado: {texto}");
+    Console.WriteLine($"Imagem gerada: {imagem}");
+}
